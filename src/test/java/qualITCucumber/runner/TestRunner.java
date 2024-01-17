a989d46174ec30c17536e6634401bb77574c8577
Kononirin <kononirin@gmail.com>
2024-01-17 12:46:08 +0500

    Refactor TestRunner

--- src/test/java/qualITCucumber/runner/TestRunner.java
+++ src/test/java/qualITCucumber/runner/TestRunner.java
@@ -1,16 +1,16 @@
 package qualITCucumber.runner;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features/")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example")
+@SelectClasspathResource("features")
+@ConfigurationParameters({
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features/"),
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example"),
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
-
+})
 public class TestRunner {
 }
